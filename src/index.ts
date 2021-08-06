db355f1dc5e1fa266942375d5bacc5a312981f52
yelo <zhihuzeye@gmail.com>
2021-08-06 17:55:15 +0800

    feat: parse requirement with Ti, Pi, Ei

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,18 @@ export const parseMemoryRequirement = (valueRaw: string | number): number => {
     if (matched) {
       return matched[1] * 1024 * 1024 * 1024
     }
+    matched = valueRaw.match(/^(\d+)Ti$/)
+    if (matched) {
+      return matched[1] * 1024 * 1024 * 1024 * 1024
+    }
+    matched = valueRaw.match(/^(\d+)Pi$/)
+    if (matched) {
+      return matched[1] * 1024 * 1024 * 1024 * 1024 * 1024
+    }
+    matched = valueRaw.match(/^(\d+)Ei$/)
+    if (matched) {
+      return matched[1] * 1024 * 1024 * 1024 * 1024 * 1024 * 1024
+    }
   }
   throw new Error('Invalid requirement format.')
 }
