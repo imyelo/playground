edd57eb55c46ab0f7c7c70b70597df5a9effd77d
yelo <zhihuzeye@gmail.com>
2021-08-06 13:47:00 +0800

    feat: parse requirement with number, Ki, Mi, Gi

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+/** 解析内存规格 */
+export const parseMemoryRequirement = (valueRaw: string | number): number => {
+  if (typeof valueRaw === 'number') {
+    return valueRaw;
+  }
+  if (typeof valueRaw === 'string') {
+    let matched;
+    matched = valueRaw.match(/^(\d+)Ki$/)
+    if (matched) {
+      return matched[1] * 1024
+    }
+    matched = valueRaw.match(/^(\d+)Mi$/)
+    if (matched) {
+      return matched[1] * 1024 * 1024
+    }
+    matched = valueRaw.match(/^(\d+)Gi$/)
+    if (matched) {
+      return matched[1] * 1024 * 1024 * 1024
+    }
+  }
+  throw new Error('Invalid requirement format.')
+}
