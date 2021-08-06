6625891dc9a6bf741cf21018adeec59aaca80dae
yelo <zhihuzeye@gmail.com>
2021-08-06 17:54:08 +0800

    test: add test about parsing requirement with Ti, Pi, Ei

--- test/test.ts
+++ test/test.ts
@@ -19,3 +19,18 @@ test('Parse Requirement with Gi', (t) => {
   t.is(parseMemoryRequirement('1Gi'), 1073741824)
   t.is(parseMemoryRequirement('8Gi'), 8589934592)
 })
+
+test('Parse Requirement with Ti', (t) => {
+  t.is(parseMemoryRequirement('1Ti'), 1099511627776)
+  t.is(parseMemoryRequirement('8Ti'), 8796093022208)
+})
+
+test('Parse Requirement with Pi', (t) => {
+  t.is(parseMemoryRequirement('1Pi'), 1125899906842624)
+  t.is(parseMemoryRequirement('8Pi'), 9007199254740992)
+})
+
+test('Parse Requirement with Ei', (t) => {
+  t.is(parseMemoryRequirement('1Ei'), 1152921504606847000)
+  t.is(parseMemoryRequirement('8Ei'), 9223372036854776000)
+})
