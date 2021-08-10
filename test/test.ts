41cddba1038fa5a3d45120eb4621ea33fa518fd8
yelo <zhihuzeye@gmail.com>
2021-08-10 18:40:54 +0800

    test: add K, M, G, T, P, E units

--- test/test.ts
+++ test/test.ts
@@ -34,3 +34,33 @@ test('Parse Requirement with Ei', (t) => {
   t.is(parseMemoryRequirement('1Ei'), 1152921504606847000)
   t.is(parseMemoryRequirement('8Ei'), 9223372036854776000)
 })
+
+test('Parse Requirement with K', (t) => {
+  t.is(parseMemoryRequirement('1K'), 1000)
+  t.is(parseMemoryRequirement('8K'), 8000)
+})
+
+test('Parse Requirement with M', (t) => {
+  t.is(parseMemoryRequirement('1M'), 1000000)
+  t.is(parseMemoryRequirement('8M'), 8000000)
+})
+
+test('Parse Requirement with G', (t) => {
+  t.is(parseMemoryRequirement('1G'), 1000000000)
+  t.is(parseMemoryRequirement('8G'), 8000000000)
+})
+
+test('Parse Requirement with T', (t) => {
+  t.is(parseMemoryRequirement('1T'), 1000000000000)
+  t.is(parseMemoryRequirement('8T'), 8000000000000)
+})
+
+test('Parse Requirement with P', (t) => {
+  t.is(parseMemoryRequirement('1P'), 1000000000000000)
+  t.is(parseMemoryRequirement('8P'), 8000000000000000)
+})
+
+test('Parse Requirement with E', (t) => {
+  t.is(parseMemoryRequirement('1E'), 1000000000000000000)
+  t.is(parseMemoryRequirement('8E'), 8000000000000000000)
+})
